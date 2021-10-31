772e7ed5efe10355268f0f5bd02b2e9fa6d3337c
Chris OBryan <cobryangithub@gmail.com>
2021-10-31 02:07:51 -0400

    VideoSource now loops array of paths

--- src/imgSources/videoSource.py
+++ src/imgSources/videoSource.py
@@ -1,5 +1,6 @@
 ''' URL-backed image source class '''
 
+from collections.abc import Iterator
 from threading import Event
 import urllib.request
 
@@ -11,17 +12,34 @@ from . source import Source
 
 class VideoSource(Source):
 
-    def __init__(self, path: str):
-        self._path: str = path
-        self._vid: cv2.VideoCapture = cv2.VideoCapture(self._path)
+    def __init__(self, paths: list[str]):
+        if isinstance(paths, str):
+            paths = [paths]
+
+        self._paths: list[str] = paths
+        self._iter: Iterator = iter(self._paths)
+        self._vidPath: str = None
+        self._vid: cv2.VideoCapture = None
+        self._nextVideo()
 
     def __repr__(self):
-        return f"VideoSource [{self._path}]"
+        return f"VideoSource [{self._vidPath}]"
+
+    def _nextVideo(self):
+        if self._vid:
+            self._vid.release()
+        self._vidPath = next(self._iter, None)
+
+        # Reset iterator when we get to the end
+        if not self._vidPath:
+            self._iter = iter(self._paths)
+            self._vidPath = next(self._iter)
+        self._vid = cv2.VideoCapture(self._vidPath)
 
     def getNextFrame(self) -> np.array:
         ret, frame = self._vid.read()
         if not ret:
-            self._vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            self._nextVideo()
             ret, frame = self._vid.read()
             if not ret:
                 return None
