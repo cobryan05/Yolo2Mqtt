387b1ff3627df71dc51a2947a756400447895708
Chris OBryan <cobryangithub@gmail.com>
2022-04-11 22:09:03 -0500

    mqttClient: Very basic subscription

--- src/mqttClient.py
+++ src/mqttClient.py
@@ -1,8 +1,15 @@
 import paho.mqtt.client as mqtt
 import time
 
+from typing import Callable
+from dataclasses import dataclass
+
 
 class MqttClient:
+    @dataclass
+    class SubMapData:
+        callback: Callable[[mqtt.MQTTMessage], None]
+
     KEY_MQTT_TOPIC = "mqtt-topic"
     KEY_MQTT_RETAIN = "mqtt-retain"
 
@@ -10,12 +17,13 @@ class MqttClient:
     CONNECTION_RETRIES = 10
     CONNECTION_RETRY_DELAY = 5
 
-    def __init__(self, broker_address, prefix="myhome/", broker_port=1883):
-        self._prefix = prefix
-        self._mqtt = mqtt.Client()
+    def __init__(self, broker_address: str, prefix: str = "myhome/", broker_port: int = 1883):
+        self._prefix: str = prefix
+        self._mqtt: mqtt.Client = mqtt.Client()
         self._mqtt.on_connect = self.mqtt_connected_callback
         self._mqtt.on_message = self.mqtt_message_callback
         self._mqtt.on_disconnect = self.mqtt_disconnect_callback
+        self._subMap: dict[str, MqttClient.SubMapData] = {}
 
         remaining_tries = MqttClient.CONNECTION_RETRIES
         while True:
@@ -37,16 +45,28 @@ class MqttClient:
     def disconnect(self):
         self._mqtt.disconnect()
 
-    def publish(self, topic, value, retain=False):
+    def publish(self, topic: str, value: str, retain: bool = False):
         publish_topic = "{}{}{}".format(self._prefix, "" if self._prefix.endswith("/") else "/", topic)
         print("Publishing {} value of {}".format(publish_topic, value))
         self._mqtt.publish(publish_topic, value, retain=retain)
 
-    def mqtt_connected_callback(self, client, userdata, flags, rc):
+    def subscribe(self, topic: str, callback: Callable[[str], None]):
+        subscribe_topic = "{}/{}".format(self._prefix.rstrip("/"), topic)
+        assert(topic not in self._subMap)
+
+        self._subMap[topic] = MqttClient.SubMapData(callback=callback)
+        self._mqtt.subscribe(subscribe_topic)
+
+    def unsubscribe(self, topic: str):
+        unsubscribe_topic = "{}{}{}".format(self._prefix, "" if self._prefix.endswith("/") else "/", topic)
+        self._mqtt.unsubscribe(unsubscribe_topic)
+        self._subMap.pop(topic)
+
+    def mqtt_connected_callback(self, client: mqtt.Client, userdata, flags, rc):
         print("MQTT server connect: {}".format(rc))
         pass
 
-    def mqtt_disconnect_callback(self, client, userdata, rc):
+    def mqtt_disconnect_callback(self, client: mqtt.Client, userdata, rc):
         print("Mqtt disconnected: {}".format(rc))
         if rc != 0:
             print("Attempting reconnect...")
@@ -55,5 +75,9 @@ class MqttClient:
             print("Stopping Mqtt loop")
             self._mqtt.loop_stop()
 
-    def mqtt_message_callback(self, client, userdata, msg):
+    def mqtt_message_callback(self, client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
         print("Received message: {}".format(msg))
+        for topic, data in self._subMap.items():
+            topic = topic.rstrip("#")
+            if msg.topic.startswith(topic) and callable(data.callback):
+                data.callback(msg)
