e00d367b4b7c871610c6585497b57e15582865ec
Chris OBryan <cobryangithub@gmail.com>
2022-05-20 10:57:04 -0500

    recordingManager: Delay start even longer

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 (RTSP_API=TRUE RTSP_APIADDRESS=0.0.0.0:9997 /usr/bin/rtsp-simple-server & \
  python3 yolo2mqtt.py --verbose "$@" & \
  python3 interactionTracker.py "$@" & \
- sleep 20 && python3 recordingManager.py --verbose "$@" )
+ sleep 60 && python3 recordingManager.py --verbose "$@" & \
+ read
+  )
